8710e5ca3e8fa5744bc5f4fe5a31da8570d7aa3e
kamzob <149818908+kamzob@users.noreply.github.com>
2024-02-12 17:36:13 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -31,9 +31,9 @@ int main() {
     for (int i = 0; i < n; i++)
     {
         int sum = 0;        // tarpiniu pazymiu suma
-        cout << "Iveskite varda:" << endl;
+        cout << "Iveskite " << i+1 << "-ojo studento varda:" << endl;
         cin >> vart[i].vardas;
-        cout << "Iveskite pavarde:" << endl;
+        cout << "Iveskite " << i+1 << "-ojo pavarde:" << endl;
         cin >> vart[i].pavarde;
         cout << "Kiek yra tarpiniu pazymiu?" << endl;
         cin >> kiek;
@@ -47,11 +47,11 @@ int main() {
         vart[i].gal = 0.4*(double(sum)/kiek)+0.6*vart[i].egz;
         
     }
-    cout << "Pavarde\t" << "Vardas\t" << "Galutinis (vid.)" << endl;
+    cout << left << setw(10) << "Pavarde" << setw(10) << "Vardas" << setw(10)<< "Galutinis (vid.)" << endl;
     cout << "---------------------------------------------------" << endl;
     for ( int i = 0; i < n; i++)
     {
-        cout << vart[i].pavarde << "\t" << vart[i].vardas << "\t" << fixed << setprecision(2) << vart[i].gal << endl;
+        cout << left << setw(10) << vart[i].pavarde << setw(10) << vart[i].vardas << setw(10) << fixed << setprecision(2) << vart[i].gal << endl;
     }
     
     
