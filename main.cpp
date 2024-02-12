9fb3d999f97d6d25858483ba95188d14f6e911d8
kamzob <149818908+kamzob@users.noreply.github.com>
2024-02-12 17:23:04 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#define x 50
 using namespace std;
 struct Vartotojas
 {
     string vardas;
     string pavarde;
-    int nd;
+    int nd[x];
     int egz;
-    double gal;
+    double gal = 0;
     
 };
 
@@ -23,34 +25,33 @@ int main() {
     int n;
     int kiek;       // tarpiniu pazymiu kiekis
     int paz;        // tarpinis pazymys
-    int sum = 0;        // tarpiniu pazymiu suma
     cout << "Iveskite vartotoju skaiciu:" << endl;
     cin >> n;
     Vartotojas vart[n];
     for (int i = 0; i < n; i++)
     {
+        int sum = 0;        // tarpiniu pazymiu suma
         cout << "Iveskite varda:" << endl;
         cin >> vart[i].vardas;
         cout << "Iveskite pavarde:" << endl;
         cin >> vart[i].pavarde;
         cout << "Kiek yra tarpiniu pazymiu?" << endl;
         cin >> kiek;
-        for (int i = 1; i <= kiek; i++){
-            cout << "Iveskite " << i << " pazymi" << endl;
-            cin >> paz;
-            sum += paz;
+        for (int j = 0; j < kiek; j++){
+            cout << "Iveskite " << j+1 << " pazymi" << endl;
+            cin >> vart[i].nd[j];
+            sum += vart[i].nd[j];
         }
-        vart[i].nd = sum/kiek;
         cout << "Iveskite egzamino rezultata:" << endl;
         cin >> vart[i].egz;
-        vart[i].gal = 0.4*vart[i].nd+0.6*vart[i].egz;
+        vart[i].gal = 0.4*(double(sum)/kiek)+0.6*vart[i].egz;
         
     }
     cout << "Pavarde\t" << "Vardas\t" << "Galutinis (vid.)" << endl;
     cout << "---------------------------------------------------" << endl;
     for ( int i = 0; i < n; i++)
     {
-        cout << vart[i].pavarde << "\t" << vart[i].vardas << "\t" << vart[i].gal << endl;
+        cout << vart[i].pavarde << "\t" << vart[i].vardas << "\t" << fixed << setprecision(2) << vart[i].gal << endl;
     }
     
     
