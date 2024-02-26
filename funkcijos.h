a6b678ae67001e194c8e997dd76f9dc51514980a
kamzob <149818908+kamzob@users.noreply.github.com>
2024-02-27 01:05:07 +0200

    Add files via upload

---
+++ funkcijos.h
@@ -0,0 +1,38 @@
+//
+//  funkcijos.h
+//  v03
+//
+//  Created by Kamilė Zobėlaitė on 2024-02-27.
+//
+
+#ifndef funkcijos_h
+#define funkcijos_h
+#include "biblioteka.h"
+struct Vartotojas
+{
+    string vardas;
+    string pavarde;
+    vector<int> nd;              // namu darbu masyvas
+    int egz;                // studento egzamino rezultatas
+    double vid = 0.0;       // namu darbu pazymiu vidurkis
+    double gal = 0.0;       // galutinis ivertinimas
+    double med = 0.0;       // namu darbu mediana
+    double galmed = 0.0;
+    double galvid = 0.0;
+    
+};
+double Vidurkis(double suma, int nariai);
+double Mediana(vector<int> paz, int nariai);
+void spausdinti(int rnkts, vector<Vartotojas>& vart, int n);
+double generavimasPaz();
+string generavimasVard(int pas);
+string generavimasPav(int pas);
+void skaityti(vector<Vartotojas>& vart);
+void rezrikiavimas(vector<Vartotojas>& vart);
+void spausdinti_skaitomus_duomenis(vector<Vartotojas>& vart);
+bool rikiuotiVarda(const Vartotojas &a, const Vartotojas &b);
+bool rikiuotiPavarde(const Vartotojas &a, const Vartotojas &b);
+bool rikiuotiVid(const Vartotojas &a, const Vartotojas &b);
+bool rikiuotiMed(const Vartotojas &a, const Vartotojas &b);
+bool arZodis(string tekstas);
+#endif /* funkcijos_h */
