d0ffd9a1fe608a2f92b002705ee0b29a1f73f6fa
kamzob <149818908+kamzob@users.noreply.github.com>
2024-03-26 03:18:59 +0200

    Add files via upload

--- funkcijos.h
+++ funkcijos.h
@@ -9,19 +9,7 @@
 #define funkcijos_h
 
 #include "biblioteka.h"
-struct Vartotojas1
-{
-    string vardas;
-    string pavarde;
-    vector<int> nd;              // namu darbu vekt
-    int egz;                // studento egzamino rezultatas
-    double vid = 0.0;       // namu darbu pazymiu vidurkis
-    double gal = 0.0;       // galutinis ivertinimas
-    double med = 0.0;       // namu darbu mediana
-    double galmed = 0.0;
-    double galvid = 0.0;
-    
-};
+
 class Vartotojas
 {
 private:
@@ -84,7 +72,7 @@ void spausdinti(int rnkts, vector<Vartotojas>& vart, int n);
 double generavimasPaz();
 string generavimasVard(int pas);
 string generavimasPav(int pas);
-void skaityti(vector<Vartotojas>& vart, string pavadinimas);
+void skaityti(vector<Vartotojas>& vart, string pavadinimas, int vm);
 void rezrikiavimas(vector<Vartotojas>& vart);
 void spausdinti_skaitomus_duomenis(vector<Vartotojas>& vart);
 bool rikiuotiVarda(const Vartotojas &a, const Vartotojas &b);
@@ -99,7 +87,7 @@ void spausdintiLaimingiVargsai (vector<Vartotojas>& vargsai, vector<Vartotojas>&
 void skaitytiList(list<Vartotojas>& vartlist, string pavadinimas, int vm);
 void RusiavimasList(list<Vartotojas>& vartlist, list<Vartotojas>& vargsailist, list<Vartotojas>& laimingilist, int vm);
 void spausdintiList (list<Vartotojas>& vargsailist, list<Vartotojas>& laimingilist, int vm);
-void skaitytiDeque(deque <Vartotojas>& vartdeq, string pavadinimas);
+void skaitytiDeque(deque <Vartotojas>& vartdeq, string pavadinimas, int vm);
 void RusiavimasDeque(deque<Vartotojas>& vartdeq, deque<Vartotojas>& vargsaideq, deque<Vartotojas>& laimingideq, int vm);
 void spausdintiDeque (deque<Vartotojas>& vargsaideq, deque<Vartotojas>& laimingideq, int vm);
 void RusiavimasDviGrupes2(vector<Vartotojas>& vart, vector<Vartotojas>& vargsai, int vm);
