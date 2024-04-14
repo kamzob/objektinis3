b8fd6a35510195d345d2ac1c04c99f34166678d4
kamzob <149818908+kamzob@users.noreply.github.com>
2024-04-14 17:03:33 +0300

    Add files via upload

---
+++ biblioteka.h
@@ -0,0 +1,49 @@
+//
+//  biblioteka.h
+//  v1_2
+//
+//  Created by Kamilė Zobėlaitė on 2024-04-14.
+//
+
+
+#ifndef biblioteka_h
+#define biblioteka_h
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <cctype>
+#include <random>
+#include <list>
+#include <deque>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::srand;
+using std::rand;
+using std::vector;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+using std::string;
+using std::sort;
+using std::istringstream;
+using std::ifstream;
+using std::ofstream;
+using std::exception;
+using std::numeric_limits;
+using std::streamsize;
+using std::runtime_error;
+using std::cerr;
+using std::list;
+using std::deque;
+#endif /* biblioteka_h */
