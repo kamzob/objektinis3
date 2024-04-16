89bb62053cceb32d2617061fd128451318cd6063
kamzob <149818908+kamzob@users.noreply.github.com>
2024-04-17 00:22:15 +0300

    Add files via upload

--- funkcijos.h
+++ funkcijos.h
@@ -10,122 +10,7 @@
 #define funkcijos_h
 
 #include "biblioteka.h"
-/*
-class Vartotojas
-{
-private:
-    string vardas_;
-    string pavarde_;
-    vector<int> nd_;              // namu darbu vekt
-    int egz_;                // studento egzamino rezultatas
-    double vid_;       // namu darbu pazymiu vidurkis
-    double gal_;       // galutinis ivertinimas
-    double med_;       // namu darbu mediana
-    double galmed_;
-    double galvid_;
-public:
-    Vartotojas() : vid_(0.0), gal_(0.0), med_(0.0), galmed_(0.0), galvid_(0.0){}
-    Vartotojas(string vardas, string pavarde, const vector<int>& nd, int egz, double vid, double gal, double med, double galmed, double galvid)
-    : vardas_(vardas), pavarde_(pavarde), nd_(nd), egz_(egz), vid_(vid), gal_(gal), med_(med), galmed_(galmed), galvid_(galvid) {}
-    // destruktorius
-    ~Vartotojas() {nd_.clear(), vardas_.clear(), pavarde_.clear();}
-    // copy konstruktorius
-    Vartotojas(const Vartotojas& other)
-    : vardas_(other.vardas_), pavarde_(other.pavarde_), nd_(other.nd_), egz_(other.egz_), vid_(other.vid_), gal_(other.gal_), med_(other.med_), galmed_(other.galmed_), galvid_(other.galvid_) {}
-    
-    // Copy assignment operatorius
-        Vartotojas& operator=(const Vartotojas& other) {
-            if (this != &other) {
-                vardas_ = other.vardas_;
-                pavarde_ = other.pavarde_;
-                nd_ = other.nd_;
-                egz_ = other.egz_;
-                vid_ = other.vid_;
-                gal_ = other.gal_;
-                med_ = other.med_;
-                galmed_ = other.galmed_;
-                galvid_ = other.galvid_;
-            }
-            return *this;
-        }
-    // move konstruktorius
-    Vartotojas(Vartotojas&& other) noexcept
-    : vardas_(std::move(other.vardas_)), pavarde_(std::move(other.pavarde_)), nd_(std::move(other.nd_)), egz_(other.egz_), vid_(other.vid_), gal_(other.gal_), med_(other.med_), galmed_(other.galmed_), galvid_(other.galvid_) {}
-    // move assignment operatorius
-    Vartotojas& operator=(Vartotojas&& other) noexcept {
-            if (this != &other) {
-                vardas_ = std::move(other.vardas_);
-                pavarde_ = std::move(other.pavarde_);
-                nd_ = std::move(other.nd_);
-                egz_ = other.egz_;
-                vid_ = other.vid_;
-                gal_ = other.gal_;
-                med_ = other.med_;
-                galmed_ = other.galmed_;
-                galvid_ = other.galvid_;
-            }
-            return *this;
-        }
-    friend ostream& operator<<(ostream& out, const Vartotojas &vart){
-        out << left << setw(20) << vart.vardas_ << setw(20) << vart.pavarde_ << setw(20) << fixed << setprecision(2) << vart.gal_ << endl;
-        return out;
-    }
-    friend istream& operator>>(istream& in, Vartotojas &vart){
-        in >> vart.vardas_ >> vart.pavarde_;
-        int paz;
-        vector <int> pzm;
-        while(in >> paz){
-            pzm.push_back(paz);
-        }
-        if (!pzm.empty()) {
-                vart.egz_ = pzm.back();
-                pzm.pop_back();
-                }
-        vart.nd_=pzm;
-        return in;
-    }
 
-    //setteriai
-    void setVar (string vard) {
-        vardas_=vard;
-    }
-    void setPav (string pav) {
-        pavarde_=pav;
-    }
-    void setPaz(int paz){
-        nd_.push_back(paz);
-    }
-    void setVid(double vidurkis){
-        vid_=vidurkis;
-    }
-    void setMed(double med){
-        med_=med;
-    }
-    void setEgz (int egz){
-        egz_ = egz;
-    }
-    void setGal(double gal){
-        gal_ = gal;
-    }
-    void setGalvid(double galv){
-        galvid_=galv;
-    }
-    void setGalmed(double galm){
-        galmed_=galm;
-    }
-    //getteriai
-    string getVar() const { return vardas_;}
-    string getPav() const {return pavarde_;}
-    const vector<int>& getPaz() const { return nd_; }
-    int getEgz() const { return egz_; }
-    double getVid() const { return vid_; }
-    double getGal() const { return gal_; }
-    double getMed() const { return med_; }
-    double getGalmed() const { return galmed_; }
-    double getGalvid() const { return galvid_; }
-    
-};
- */
 class Zmogus
 {
 protected:
@@ -136,15 +21,16 @@ public:
     Zmogus(string vardas, string pavarde)
     : vardas_(vardas), pavarde_(pavarde) {}
     virtual ~Zmogus() {}
-    virtual void  kazkas() const = 0;
+   // virtual void  kazkas() const = 0;
     virtual void setVar (string vard) {
         vardas_=vard;
     }
     virtual void setPav (string pav) {
         pavarde_=pav;
     }
-    virtual string getVar() const { return vardas_;}
-    virtual string getPav() const {return pavarde_;}
+
+    virtual string getVar() const = 0;
+    virtual string getPav() const = 0;
 };
 class Vartotojas : public Zmogus
 {
@@ -161,11 +47,14 @@ public:
     Vartotojas(const string vardas, const string pavarde, const vector<int>& nd, int egz, double vid, double gal, double med, double galmed, double galvid)
     : Zmogus(vardas, pavarde), nd_(nd), egz_(egz), vid_(vid), gal_(gal), med_(med), galmed_(galmed), galvid_(galvid) {}
     // destruktorius
-    ~Vartotojas() { nd_.clear(); }
+    ~Vartotojas() {
+        cout << "Objektas sunaikintas" << endl;
+        nd_.clear();
+        }
     // copy konstruktorius
     Vartotojas(const Vartotojas& other)
     : Zmogus(other.getVar(), other.getPav()), nd_(other.nd_), egz_(other.egz_), vid_(other.vid_), gal_(other.gal_), med_(other.med_), galmed_(other.galmed_), galvid_(other.galvid_) {}
-    void kazkas () const override{}
+    //void kazkas () const override{}
     
     // Copy assignment operatorius
         Vartotojas& operator=(const Vartotojas& other) {
@@ -250,8 +139,8 @@ public:
     double getMed() const { return med_; }
     double getGalmed() const { return galmed_; }
     double getGalvid() const { return galvid_; }
-    string getVar() const override { return Zmogus::getVar(); }
-    string getPav() const override { return Zmogus::getPav(); }
+    string getVar() const override { return vardas_; }
+    string getPav() const override { return pavarde_; }
     
     
 };
