bf15e808c5f7a63f57a7f48ed05c0b1225a9ce77
kamzob <149818908+kamzob@users.noreply.github.com>
2024-05-15 20:35:35 +0300

    Add files via upload

---
+++ biblioteka.h
@@ -0,0 +1,65 @@
+//
+//  biblioteka.h
+//  V3.0
+//
+//  Created by Kamilė Zobėlaitė on 2024-05-15.
+//
+
+//
+//  biblioteka.h
+//  v2.0
+//
+//  Created by Kamilė Zobėlaitė on 2024-04-29.
+//
+/**
+ * @file biblioteka.h
+ * @brief biblioteku deklaracija
+ */
+
+#ifndef biblioteka_h
+#define biblioteka_h
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <cctype>
+#include <random>
+#include <list>
+#include <deque>
+#include <utility>
+#include <assert.h>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::srand;
+using std::rand;
+using std::vector;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+using std::string;
+using std::sort;
+using std::istringstream;
+using std::ifstream;
+using std::ofstream;
+using std::exception;
+using std::numeric_limits;
+using std::streamsize;
+using std::runtime_error;
+using std::cerr;
+using std::list;
+using std::deque;
+using std::move;
+using std::ostream;
+using std::istream;
+using std::accumulate;
+#endif /* biblioteka_h */
