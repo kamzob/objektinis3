1f1eb2f9e456093791dafebe11e211c4752b3f5f
kamzob <149818908+kamzob@users.noreply.github.com>
2024-05-15 20:36:43 +0300

    Add files via upload

---
+++ vektorius.h
@@ -0,0 +1,21 @@
+//
+//  vector.h
+//  V3.0
+//
+//  Created by Kamilė Zobėlaitė on 2024-05-15.
+//
+
+#ifndef vektorius_h
+#define vektorius_h
+
+#include "biblioteka.h"
+class Vektorius
+{
+private:
+    T* mduom_; // Dinaminis masyvas, saugantis duomenis
+    size_type mdydis_; // Konteinerio dydis (dabar kiek elementu yra vektoriuje)
+    size_type mtalpa_; // Talpa (kiek elementų gali būti saugoma)
+public:
+    
+};
+#endif /* vektorius_h */
